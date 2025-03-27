0a29618d7975338eaf01ff6b298aa86928aeb084
AlejandroAriasSL <alejandroasilva02@gmail>
2025-03-27 12:17:00 +0100

    test_should_return_name_of_station() (tdd:green)

---
+++ src/main/java/dev/alejandro/models/Station.java
@@ -0,0 +1,15 @@
+package dev.alejandro.models;
+
+public class Station {
+
+    String name;
+
+    public Station (String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+}
