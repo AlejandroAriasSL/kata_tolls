47d9f7607abad3bf76addd64bf4433b7094fccb3
AlejandroAriasSL <alejandroasilva02@gmail>
2025-03-27 12:31:45 +0100

    test_should_return_city_of_station() (tdd:green)

--- src/main/java/dev/alejandro/models/Station.java
+++ src/main/java/dev/alejandro/models/Station.java
@@ -2,14 +2,19 @@ package dev.alejandro.models;
 
 public class Station {
 
-    String name;
+    String name, city;
 
-    public Station (String name) {
+    public Station (String name, String city) {
         this.name = name;
+        this.city = city;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getCity() {
+        return city;
     }
     
 }
